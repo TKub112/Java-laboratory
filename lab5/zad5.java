4d7bc53e3b8be3a367de2236dcb4527450891507
Tobiasz <tobiaszkubiak@gmail.com>
2019-04-07 01:25:22 +0200

    ez

---
+++ lab5/zad5.java
@@ -0,0 +1,118 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.lang.Exception;
+import java.util.InputMismatchException;
+import java.io.File; 
+import java.util.Random;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.concurrent.TimeUnit;
+import java.lang.String;
+import java.io.FileNotFoundException;
+import java.lang.StringIndexOutOfBoundsException;
+/*
+optymalizacja kodu pod wzgledem wczytywania, zeby dzialalo podobnie
+popraw javanio func
+https://www.tutorialspoint.com/java_nio/java_nio_file.htm
+*/
+public class zad5
+{
+ public static void main(String []args) 
+   {
+			
+			/*File myObj = new File("filename.txt"); // Specify the filename
+				if (myObj.createNewFile()) {
+		      System.out.println("File created: " + myObj.getName());
+		    } else {
+		      System.out.println("File already exists.");
+		    } */
+			try
+			{
+
+					Scanner scan= new Scanner(System.in);
+					System.out.println("Input filename:");
+					String str=scan.nextLine();
+					//giving input filename
+
+
+					File myObj = new File("name_of_file.txt"); // Specify the filename
+					myObj.createNewFile();
+					FileWriter myWriter = new FileWriter("name_of_file.txt");
+					PrintWriter writer = new PrintWriter(myObj);
+				  writer.print("");
+					myWriter.write(str);
+				  myWriter.close();
+					//creating and writing file
+		
+					String str_data="";
+					BufferedReader br = null;
+					FileReader fr = null;
+					fr = new FileReader(str);
+					br = new BufferedReader(fr);
+					String sCurrentLine;
+					
+					while ((sCurrentLine = br.readLine()) != null) 
+					{
+						str_data+=sCurrentLine;
+						str_data+="\n";
+					}
+					//reading file to str_data
+					System.out.println(str_data);
+					//czytanie z pliku 
+					Random r = new Random();
+					int str_len,randomv;
+
+					while(true)
+					{
+					System.out.println("Press Enter to continue");
+					scan.nextLine();
+					str_len=r.nextInt(str_data.length());
+					randomv=r.nextInt(5);
+					if(str_len+randomv>str_data.length())
+					{
+					str_len-=5;
+					}
+					for(int i=0;i<randomv;i++)
+					{
+						
+						System.out.println((char)str_data.charAt(str_len+i));
+					}
+					System.out.println("");
+					}
+					
+
+			}
+			catch(FileNotFoundException s)
+			{System.out.println("File not exists");
+			}
+			catch(ArrayIndexOutOfBoundsException a)
+			{
+		  	System.out.println("You missed arguments");
+			}
+			catch(InputMismatchException b)
+			{
+		  	System.out.println("You didnt entered number");
+			}
+			catch (IOException e) 
+			{
+      System.out.println("An error occurred.");
+      e.printStackTrace(); 
+			}
+			catch(StringIndexOutOfBoundsException a1)
+			{
+		  	System.out.println("Out of index");
+			}
+			
+			
+			
+		}
+   
+	
+
+
+
+}
+
--- lab6/scratch.java
+++ lab6/scratch.java
@@ -1,24 +1,41 @@
 import java.util.Scanner;
-
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 class scratch {
 
     public static void main(String[] args) {
+        int[] arr_A;
+        int[] arr_B;
 
         while(true) {
-            int[] arr_A;
-            int[] arr_B;
+
             arr_A = read_vector();
             arr_B = read_vector();
             int size_a = arr_A[0], size_b = arr_B[0];////the first element of vector is the length of vector
-        if(size_a[0]==size_b[0]) {
-            if(size_a[0]==0) {
-                System.out.print("vector are ampty")
+            if(size_a==size_b) {
+                if(size_a==0) {
+                System.out.print("vector are ampty");
+                }
+                break;
+             }
+            else
+            {
+                continue;
             }
-            break;
-        }
         }// Input vector with the same siz
-        
+
+
+
+        int[] sum_vec;
+        sum_vec = new int[arr_A[0]];
+        sum_vec[0]=arr_A[0];////the first element of vector is the length of vector
+        for(int i=1;i<arr_A[0]+1;i++)
+        {
+            sum_vec[i]=arr_A[i]+arr_B[i];
+        }
+
+        write_vector_tofile(sum_vec);
 
 
 
@@ -31,11 +48,16 @@ class scratch {
         while(true){
             String userName = myObj.nextLine();  // Read user input
             String[] integerStrings = userName.split(" ");
-            integers = new int[integerStrings.length];
-            integers[0]=integers.length;//saving on the first element of vector the length of vector
+            System.out.print(userName);
+            System.out.print(userName);
+            integers = new int[integerStrings.length+1];
+            integers[0]=integerStrings.length-1;//saving on the first element of vector the length of vector
+            System.out.print(integers[0]);
             try {
                 for (int i = 1; i < integers.length+1; i++) {
                     integers[i] = Integer.parseInt(integerStrings[i]);
+                    System.out.print(i);
+                    System.out.print(integers[i]);
                 }
             }
             catch(java.lang.NumberFormatException a)
@@ -48,4 +70,22 @@ class scratch {
 
         return integers;
 }
+
+
+
+    public static void write_vector_tofile(int[] data) {
+
+
+
+        try {
+            FileOutputStream file = new FileOutputStream("pic.dat");
+            for (int i = 0; i < data[0]; i++)
+                file.write(data[i]);
+            file.close();
+        } catch (IOException e) {
+            System.out.println("Error - " + e.toString());
+        }
+    }
+
+
 }
